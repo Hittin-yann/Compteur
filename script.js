aa5a89a7fdd974b36d1021915b3feb4e14aa42a9
Yann Hittin <hittinyann@gmail.com>
2024-06-13 12:32:28 +0200

    clue

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const countdown = () => {
 function clue (days, hours) {
   if (days == 0 & hours < 23) {
     document.getElementById("compteur").innerHTML =+ 
-    "<br /> Mystère n°1 : 🎮"+
+    "<br /> Mystère n°1 : 🎮 🟢"+
     "<br /> Mystère n°2 : ❤️ 📖 🌐"
   }
 }
